71f62582b558166a9a936d7a0d8cf50735d719aa
Michelle Lovse <67603844+mlovse1@users.noreply.github.com>
2022-08-08 17:52:20 -0500

    Updated wrong files

--- src/main/java/com/cognixia/jump/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognixia.jump.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognixia.jump.model.Orders;
-
-public interface OrderRepository extends JpaRepository<Orders, Integer> {
-
-}
